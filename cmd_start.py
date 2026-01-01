6a1d62c0c48ba003fc8fabb0cbb16b8649d0ba46
SIKO-1 <156326838+SIKO-1@users.noreply.github.com>
2026-01-01 23:23:05 +0300

    Delete cmd_start.py

--- cmd_start.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes
-
-# الدالة التي تنفذ عند كتابة /start
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("أهلاً بك! أنا العقل المدبر.. تم تفعيل الربط التلقائي بنجاح ✅")
-
-# بدلاً من handler = ...
-start_handler = CommandHandler("start", start_command)

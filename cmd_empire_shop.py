9e8fa6a1869d1a422285fb75b4135864cd29c457
SIKO-1 <156326838+SIKO-1@users.noreply.github.com>
2026-01-03 01:16:49 +0300

    Delete cmd_empire_shop.py

--- cmd_empire_shop.py
+++
@@ -1,5 +0,0 @@
-def register_shop_handlers(bot):
-    
-    @bot.message_handler(func=lambda m: m.text in ["متجر", "المتجر"])
-    def empire_shop(m):
-        bot.reply_to(m, "👑 تم تفعيل متجر الإمبراطورية الجديد بنجاح!")

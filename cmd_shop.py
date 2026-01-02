d0feabb55fc23e7f94b0e5176e8703ee724d029c
SIKO-1 <156326838+SIKO-1@users.noreply.github.com>
2026-01-02 18:27:31 +0300

    Delete cmd_shop.py

--- cmd_shop.py
+++
@@ -1,5 +0,0 @@
-def register_shop_handlers(bot):
-    
-    @bot.message_handler(func=lambda m: m.text in ["متجر", "المتجر", "شوب", "shop"])
-    def send_welcome_shop(m):
-        bot.reply_to(m, "اهلا بك في المتجر")
